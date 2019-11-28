4754079a24e6c7d905dddfbfa1c71a98a542d092
Daniel Schrøder <schroeder1004@hotmail.com>
2019-11-28 11:51:18 +0100

    Entities added
    Style, tempo, composition

---
+++ EksamensProject/EksamensProject.Core.Entity/Composition.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EksamensProject.Core.Entity
+{
+    public class Composition
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public DateTime Year { get; set; }
+        public Double Duration { get; set; }
+        
+    }
+}
---
+++ EksamensProject/EksamensProject.Core.Entity/Style.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EksamensProject.Core.Entity
+{
+    public class Style
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Era { get; set; }
+        
+    }
+}

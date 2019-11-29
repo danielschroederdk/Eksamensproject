7765ab1ac19b40358145e323b415469f3c049e35
Daniel Schrøder <schroeder1004@hotmail.com>
2019-11-29 14:17:44 +0100

    CompositionController added
    +CompositionController

---
+++ EksamensProject/EksamensProjectRestApi/Controllers/CompositionController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using EksamensProject.Core.ApplicationService;
+using EksamensProject.Core.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EksamensProjectRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompositionController : ControllerBase
+    {
+        private readonly ICompositionService _compositionService;
+
+        public CompositionController(ICompositionService compositionService)
+        {
+            _compositionService = compositionService;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Composition>> Get()
+        {
+            try
+            {
+                return _compositionService.GetCompositions();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Composition> Get(int id)
+        {
+            try
+            {
+               return Ok(_compositionService.FindCompositionById(id));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(404, $"owner {id} not found");
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<Composition> Post([FromBody] Composition composition)
+        {
+            try
+            {
+                return Ok(_compositionService.CreateComposition(composition));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult<Composition> Put(int id, [FromBody] Composition composition)
+        {
+            if (id != composition.Id)
+                return BadRequest("ID does not coincide");
+            
+            return Ok(_compositionService.UpdateComposition(composition));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<User> Delete(int id)
+        {
+            var toRemove = _compositionService.Delete(id);
+            return toRemove == null ? StatusCode(404, $"Composition {id}  not found") : Ok($"Composition: {toRemove.Name} (ID: {id}) deleted");
+        }
+    }
+}

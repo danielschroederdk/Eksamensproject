98d7fff8cdb4b72832adc9b2dddd6cc8ea6d4c48
Alex Uldahl Pedersen <alex@uldahl.dk>
2019-12-10 18:45:45 +0100

    FindById

---
+++ EksamensProject/EksamensProject.Infrastructure.SQL/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using EksamensProject.Core.DomainService;
+using EksamensProject.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EksamensProject.Infrastructure.SQL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        readonly EksamensProjectContext _ctx;
+
+        public UserRepository(EksamensProjectContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public User Create(User user)
+        {
+            _ctx.Add(user);
+            _ctx.SaveChanges();
+            return user;
+        }
+
+        public User ReadById(int id)
+        {
+            return _ctx.Users.Find(id);
+        }
+
+        public IEnumerable<User> ReadAll()
+        {
+            return _ctx.Users.ToList();        
+        }
+
+        public User Update(User userUpdate)
+        {
+            _ctx.Entry(userUpdate).State = EntityState.Modified;
+            _ctx.SaveChanges();
+            return userUpdate;
+        }
+
+        public User Delete(int id)
+        {
+            var userToDelete = ReadById(id);
+            _ctx.Users.Remove(userToDelete);
+            _ctx.SaveChanges();
+            return userToDelete;
+        }
+    }
+}
